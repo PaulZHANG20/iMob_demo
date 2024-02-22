5a39777ce623995725bd306ebcc7bef5cc15f398
YL <724617247@qq.com>
2024-02-22 16:56:06 +0800

    file for running

---
+++ Run_iMoB.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+##******************************************************************************//
+## * Project Director: Prof. Lining Zhang                                        //
+## * Authors: Ying Ma, Yu Li.                                                    //
+## * Notes: Running iMoB demo program.                               //
+## ******************************************************************************//
+
+import subprocess
+
+# Run imob open source.py
+file_to_run = 'imob_open_source.py'
+subprocess.run(['python', file_to_run], check=True)
